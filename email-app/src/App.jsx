09e90cef08e38c85f4dbe9fa8fcd3090a4b84c8c
Hector Martinez <hec.gre81@gmail.com>
2024-05-25 23:41:38 -0600

    Add logic for tensorflow.js

--- email-app/src/App.jsx
+++ email-app/src/App.jsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react'
+import * as tf from '@tensorflow/tfjs'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [model, setModel] = useState(null)
+  const [prediction, setPrediction] = useState(null)
+
+  useEffect(() => {
+    // Cargar el modelo convertido
+    const loadModel = async () => {
+      const model = await tf.loadLayersModel('/model-js/model.json')
+      setModel(model)
+    }
+
+    loadModel()
+  }, [])
+
+  const makePrediction = async (inputData) => {
+    if (model) {
+      // Tokenizar el texto
+      const inputTensor = tf.tensor([inputData])
+      const output = model.predict(inputTensor)
+      const prediction = output.dataSync()
+      setPrediction(prediction)
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>TensorFlow.js en React</h1>
+      <button onClick={() => makePrediction('text')}>
+        Hacer Predicción
+      </button>
+      {prediction && <div>Predicción: {prediction}</div>}
+    </div>
   )
 }
 

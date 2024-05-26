23d57c310fdfded95c32061b3bedceadfe2f7ada
Hector Martinez <hec.gre81@gmail.com>
2024-05-26 01:48:24 -0600

    Add handleSubmit for the event email

--- email-app/src/components/Email/index.jsx
+++ email-app/src/components/Email/index.jsx
@@ -3,6 +3,15 @@ import styles from './styles.module.css'
 
 const Email = () => {
   const [isVisible, setIsVisible] = useState(true)
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    setTitle('')
+    setContent('')
+  }
   return (
     <aside className={styles.email_content}>
       <section className={styles.email_header}>
@@ -13,16 +22,34 @@ const Email = () => {
 
       </section>
       {isVisible && (
-        <section className={styles.email_container}>
+        <form className={styles.email_container} onSubmit={handleSubmit}>
           <p>
             <strong>Para: </strong>
             <span>
               you@ClasifAI.com
             </span>
           </p>
-          <input type='text' name='subject' placeholder='Título' />
-          <textarea name='content' id='content' cols='30' rows='10' placeholder='Contenido' />
-        </section>
+          <input
+            type='text'
+            name='subject'
+            placeholder='Título'
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+          <textarea
+            name='content'
+            id='content'
+            cols='30'
+            rows='10'
+            placeholder='Contenido'
+            onChange={(e) => setContent(e.target.value)}
+            value={content}
+          />
+
+          <button className={styles.button}>
+            Enviar
+          </button>
+        </form>
 
       )}
     </aside>
